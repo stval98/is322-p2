465e11f8585f9770811bb0afae6549e7e6276a0b
sgupta82297 <47404972+sgupta82297@users.noreply.github.com>
2019-04-14 22:07:12 -0400

    Create addtask.js

---
+++ assignment02/src/addtask.js
@@ -0,0 +1,13 @@
+import React from 'react';
+class AddTask extends React.Component {
+state={NewTask}
+Submit(NewTask);
+({NewTask})
+}
+return (
+<className="TaskInput" 
+Submit={}>
+<Enter Task>
+name="newTask"
+}
+}
