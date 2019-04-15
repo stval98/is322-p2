90dd300258c4a28cbb7a348b48317378a076a55d
sgupta82297 <47404972+sgupta82297@users.noreply.github.com>
2019-04-14 22:24:14 -0400

    Create app.js

---
+++ assignment02/src/app.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import axios from 'axios';
+import TaskList from './TaskList';
+import AddTask from './AddTask';
+class extends React.Component {
+state = {
+tasks: [],
+getData() {
+axios.get(https://my-json-server.typicode.com/stval98/is322-p2/posts')
+({ tasks: response.data });
+AddTask = (taskName) {}
+tasks = tasks;
+title: taskName,
+type: 'task',
+column: 'todo'}
+TaskList = (newTask)
+render() {
+return (
+<Task ={this.Task} >
+<TaskList tasks={}
+}
+}
